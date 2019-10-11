252f6803daa6477bae9d455a5b54c57dbbc3e112
yalin.jiang <yalin.jiang@71360.com>
2019-10-11 14:34:29 +0800

    xx

--- content.js
+++ content.js
@@ -6,11 +6,23 @@ const convert = require('xml-js')
 
 const updatedList = fs.readFileSync('./files/chapter.json', 'utf8')
 
-const node = JSON.parse(updatedList).node[0].node
+// const node = JSON.parse(updatedList).node
+const node = [{
+  bookTitle: 'xx',
+  node: [JSON.parse(updatedList).node[0].node[0]]
+}]
+let word = []
+
+word = node.map(item => {
+  return {
+    bookName: item.bookTitle,
+    node: new Array(item.node.length)
+  }
+})
 
 const crawler = new Crawler({
   encoding: null, // 编码
-  maxConnections: 5, // 最大并发请求数
+  maxConnections: 10, // 最大并发请求数
   callback: (err, res, done) => {
     if (err) console.log(err)
     if (!err) console.log(res.options)
@@ -19,25 +31,26 @@ const crawler = new Crawler({
 })
 
 const urls = []
-node.forEach(item => {
-  let url = {
-    uri: item.href,
-    jQuery: true,
-
-    callback: (err, res, done) => {
-      if (err) console.log(err)
-      if (!err) getTotalPageMain(res, item)
-      done()
+node.forEach(data => { // book
+  data.node.forEach((item, i) => { // 章
+    let url = {
+      uri: item.href,
+      jQuery: true,
+      callback: (err, res, done) => {
+        if (err) console.log(err)
+        if (!err) getTotalPageMain(res, i + 1, item)
+        done()
+      }
     }
-  }
-  urls.push(url)
+    urls.push(url)
+  })
 })
 
 let total = 0 // 所有book的总页数
 let thisEndPage = 0 // 当前完成页数
 let chapter = []
 
-const getTotalPageMain = (res, joint) => {
+const getTotalPageMain = (res, serialNumber, joint) => {
   const xml = res.body.toString()
   const json = convert.xml2json(xml, {compact: false, spaces: 4})
   const node = JSON.stringify(json) // 返回的结构
@@ -49,12 +62,12 @@ const getTotalPageMain = (res, joint) => {
       node: new Array(totalPage)
     })
   }
-  const crawlerPageUrls = GetCrawlerPageUrl(joint, totalPage)
+  const crawlerPageUrls = GetCrawlerPageUrl(serialNumber, joint, totalPage)
   crawler.queue(crawlerPageUrls)
 }
 
 // 生成每页对象
-const GetCrawlerPageUrl = (joint, totalPage) => {
+const GetCrawlerPageUrl = (serialNumber, joint, totalPage) => {
   const arr = []
   for (let i = 0; i < Number(totalPage); i++) {
     arr.push({
@@ -62,7 +75,7 @@ const GetCrawlerPageUrl = (joint, totalPage) => {
       jQuery: true,
       callback: (err, res, done) => {
         if (err) console.log(err)
-        if (!err) main(res, joint, i + 1)
+        if (!err) main(res, serialNumber, joint, totalPage, i + 1)
         done()
       }
     })
@@ -70,47 +83,38 @@ const GetCrawlerPageUrl = (joint, totalPage) => {
   return arr
 }
 
-const main = (res, joint, page) => {
+const main = (res, serialNumber, joint, totalPage, page) => {
   thisEndPage ++
   const xml = res.body.toString()
   const json = convert.xml2json(xml, {compact: false, spaces: 4})
+  // console.log('joint', joint)
   const node = JSON.stringify(json) // 返回的结构
-  fs.writeFileSync('./files/text/' + joint.name + ' ' + page + '.html', xml)
-  // const text = GetUpdatedList(JSON.parse(JSON.parse(node))) // 分页
-  console.log('node', node)
-  // chapter = chapter.map(item => {
-  //   let node = item.node
-  //   if (item.bookTitle === book.title) {
-  //     node[page - 1] = text
-  //   }
-  //   return {
-  //     ...item,
-  //     node
-  //   }
-  // })
-  // if (thisEndPage === total) {
-  //   setTimeout(() => {
-  //     chapter = chapter.map(item => {
-  //       const node = []
-  //       item.node.forEach(data => {
-  //         data.forEach(arr => {
-  //           node.push({
-  //             ...arr,
-  //             bookName: item.bookTitle
-  //           })
-  //         })
-  //       })
-  //       return {
-  //         ...item,
-  //         node
-  //       }
-  //     })
-  //     fs.writeFileSync('./files/chapter.json', JSON.stringify({
-  //       tips: '章节',
-  //       node: chapter
-  //     }))
-  //   }, 1000)
-  // }
+  fs.writeFileSync('./files/text/' + joint.name + ' ' + page + '.html', xml) // 记录内容页
+  const txtGroup = GetContentTxtGroup(JSON.parse(JSON.parse(node))) // 分页
+  const txt = TxtGroupToContent(txtGroup)
+  // joint.bookName
+  // serialNumber 节序号
+  // joint.name // 节名
+  // totalPage // 总页
+  // page 页
+  word = word.map(item => {
+    const node = item.node
+    if ('xx' === item.bookName) {
+      if (!node[serialNumber - 1]) node[serialNumber - 1] = new Array(totalPage)
+      console.log('node[serialNumber - 1][page - 1]', totalPage === 3)
+      node[serialNumber - 1][page - 1] = txt
+    }
+    return {
+      ...item,
+      node
+    }
+  })
+  if (thisEndPage === total) {
+    console.log('aa', word)
+    console.log('word.node', word.node)
+  }
+
+  // fs.writeFileSync('./files/txt/' + '测试' + ' - ' + page + '.txt', txt) // 记录分页内容
 }
 
 // 获取总页数
@@ -120,10 +124,29 @@ const GetTotalPage = json => {
   return text ? Number(text.substring(text.indexOf('/') + 1, text.length - 1)) : 0
 }
 
-// 获取最新更新的分页列表（格式化后的xml的json结构）
-const GetUpdatedList = json => {
-  const newBook = json.elements[1].elements[1].elements[0].elements.find(item => item.elements)
-  return 'http://www.wenku8.net/wap/article/' + newBook.attributes.href
+// 获取内容节点
+const GetContentTxtGroup = json => {
+  let observationEnd = false // 是否观测到结束标记
+  return json.elements[1].elements[1].elements[0].elements.filter(item => {
+    if (item.type === 'text') {
+      const isMarkNode = (item.text.indexOf('[') === 0 && item.text.indexOf(']') === item.text.length - 1)
+      if (isMarkNode && !observationEnd) {
+        observationEnd = true
+      } else if (isMarkNode && observationEnd) {
+        observationEnd = false
+      }
+    }
+    return item.type === 'text' && observationEnd
+  })
+}
+
+// 将内容节点转化为内容
+const TxtGroupToContent = group => {
+  let text = ''
+  group.forEach(item => {
+    text += item.text
+  })
+  return text
 }
 
 crawler.queue(urls)
